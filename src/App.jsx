b5f05717ef6e4653ebb93542c579b48d18261153
Khatuk <ximena0301@gmail.com>
2023-03-08 10:19:42 -0500

    Inicio repositorio primer parcial Frontend III

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Card from './Components/Card'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  )
+}
+
+export default App
